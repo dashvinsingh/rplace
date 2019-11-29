a0d6c41d2f91327dcde628fba996980a091750f9
Bhavjot Baidwan <b.baidwan@mail.utoronto.ca>
2019-11-28 23:16:18 -0500

    putItem and getItem working

--- place/database/db_api.js
+++ place/database/db_api.js
@@ -3,19 +3,39 @@ AWS.config.update({region: 'us-east-2'});
 // Create the DynamoDB service object
 var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
-var params = {
-  TableName: 'Canvas',
-  Item: {
-    'Coordinate' : {S: "1,1"},
-    'Colour' : {B: "0000"}
-  }
-};
 
 // Call DynamoDB to add the item to the table
-ddb.putItem(params, function(err, data) {
-  if (err) { //Error
-    console.log("Error", err);
-  } else { // Successful Respose
-    console.log("Success", data);
+function putItem(item, tableName) {
+  var params = {
+    TableName: tableName,
+    Item: item
   }
-});
+  ddb.putItem(params, function(err, data) {
+    if (err) { //Error
+      console.log("Error", err);
+    } else { // Successful Respose
+      console.log("Success", data);
+    }
+  });
+}
+
+function getItem(key, tableName){
+  var params = {
+    Key: key,
+    TableName: tableName
+  }
+  ddb.getItem(params, function(err, data) {
+      if(err){
+        console.log("Error", err)
+      } else {
+        console.log("Success", data)
+      }
+  });
+}
+
+// putItem({'Coordinate' : {S: "44,1"},
+//     'Colour' : {B: "1100"}}, "Canvas");
+// putItem({'User' : {S: "julian"},
+//     'Token' : {S: "2"}}, "Users");
+// getItem({'User' : {S: "julian"},
+// 'Token' : {S: "2"}}, "Users");
