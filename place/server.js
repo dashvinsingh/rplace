860825c6884e7f61e9aff2f42a2ccdc29f075de1
Julian-Sequeira <julian.sequeira@mail.utoronto.ca>
2019-12-03 17:15:49 -0500

    Added session functionality

--- place/server.js
+++ place/server.js
@@ -1,12 +1,6 @@
 const WebSocket = require('ws');
-const dotenv = require('dotenv');
-var redis = require("redis")
-dotenv.config();
-
-const env = process.env;
-const wss = new WebSocket.Server({ port: env.WS_PORT });
-const redisClient = redis.createClient("redis://r-place-redis-cache-001.tjlnvm.0001.use2.cache.amazonaws.com", {return_buffer:true});
-
+const wss = new WebSocket.Server({ port: 8081 });
+ 
 var dim = 250; // note: this is not the right dimensions!!
 var board=new Array(dim);
 for(var x=0;x<dim;x++){
@@ -28,7 +22,6 @@ wss.broadcast = function broadcast(data) {
   });
 };
 
-
 // for heartbeat to make sure connection is alive 
 function noop() {}
 function heartbeat() {
@@ -49,75 +42,27 @@ function isValidSet(o){
 	} 
 	return isValid;
 }
-
-function send(ws, command, payload) {
-	ws.send(JSON.stringify({command: command, payload: payload}))
-}
-
-function readBytes(byteString) {
-	buff = Buffer.from(byteString);
-	var board = [];
-	for (var i=0; i<buff.length; i++) {
-			board.push(buff[i]);
-	}
-	return board;
-}
-
-const redisKey = "board";
 wss.on('connection', function(ws) {
 	// heartbeat
-	console.log("New Socket Connection");
   	ws.isAlive = true;
   	ws.on('pong', heartbeat);
-	
-	//Client first connects to websocket
-	//Need to send the redit board here.
-	redisClient.send_command("GET", [redisKey], function(err, reply) {
-		if (err) {console.log(err); send(ws, "ERROR", {"message": err})};
-		if (reply) {
-				console.log(`REDIS GET ${redisKey}, Size: ${reply.length}`);
-				send(ws, "INIT", {board: readBytes(reply)});
-		}
-	});
 
 	// send initial board: this is slow!!!
-	// for (x=0;x<dim;x++){
-	// 	for(y=0;y<dim;y++){
-	// 		var o = { 'x' : x, 'y' : y, 'r': board[x][y].r, 'g': board[x][y].g, 'b': board[x][y].b };
-	// 		ws.send(JSON.stringify(o));
-	// 	}
-	// }
+	for(x=0;x<dim;x++){
+		for(y=0;y<dim;y++){
+			var o = { 'x' : x, 'y' : y, 'r': board[x][y].r, 'g': board[x][y].g, 'b': board[x][y].b };
+			ws.send(JSON.stringify(o));
+		}
+	}
 
 	// when we get a message from the client
 	ws.on('message', function(message) {
+		console.log(message);
 		var o = JSON.parse(message);
-		switch(o.command) {
-			case "INIT":
-				console.log(o)
-				break;
-			case "WRITE":
-				console.log(o.payload);
-				offset = parseInt(o.payload.x) + parseInt(o.payload.y) * 250;
-				console.log(offset);
-				value = parseInt(o.payload.num);
-				console.log("New data to write from user " + o.payload.user);
-				redisClient.send_command("BITFIELD", [redisKey, "SET", "u8", `#${offset}`, value], function(err, reply) {
-        				if (err) console.log(err);
-        				console.log(reply);
-					wss.broadcast(JSON.stringify({command:"READ", payload:o}));;	
-				});	
-				//Validate if valid input (color validation)
-				//Write to dynamo
-				//Write to redis
-				//Broadcast to all clients
-				// if (isValidSet(o)){
-				// 	wss.broadcast(message);
-				// 	board[o.x][o.y] = { 'r': o.r, 'g': o.g, 'b': o.b };
-				// }
-			default:
-				break;
+		if(isValidSet(o)){
+			wss.broadcast(message);
+			board[o.x][o.y] = { 'r': o.r, 'g': o.g, 'b': o.b };
 		}
-
 	});
 });
 
@@ -140,31 +85,26 @@ var app = express();
 app.use('/',express.static('static_files')); // this directory has files to be returned
 
 
-// Code to handle session functionality: made using https://glebbahmutov.com/blog/express-sessions/
+// Session stuff taken from: https://glebbahmutov.com/blog/express-sessions/
+app.use(require('morgan')('dev'));
 const session = require('express-session');
-const FileStore = require('session-file-store')(session)
+const FileStore = require('session-file-store')(session);
 
 app.use(session({
-	name: 'server-session-cookie-id',
-	secret: 'session secret',
-	saveUninitialized: true,
-	resave: true,
-	store: new FileStore()
+  name: 'server-session-cookie-id',
+  secret: 'my express secret',
+  saveUninitialized: true,
+  resave: true,
+  store: new FileStore()
 }));
+
 
 app.use(function printSession(req, res, next) {
 	console.log('req.session', req.session);
 	return next();
   });
 
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
 
-
-
-
-
-
-// app.listen(8080, function () {
-//   console.log('Example app listening on port 8080!');
-app.listen(env.APP_PORT, function () {
-  console.log(`Example app listening on port ${env.APP_PORT}!`);
-})
