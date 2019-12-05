3c46910c5b689b941eb3e80b1dba2cff9a8fb30a
Bhavjot Baidwan <b.baidwan@mail.utoronto.ca>
2019-12-05 01:30:34 -0500

    Can connect to RDS

---
+++ updateApi/db.js
@@ -0,0 +1,27 @@
+require('log-timestamp');
+const { Pool, Client } = require('pg');
+const pool = new Pool({
+    user: 'postgres',
+    host: 'r-place-1.cg7a8becuhbz.us-east-2.rds.amazonaws.com',
+    database: 'Canvas',
+    password: 'csc409place',
+    port: 5432
+})
+
+const client = new Client({
+    user: 'postgres',
+    host: 'r-place-1.cg7a8becuhbz.us-east-2.rds.amazonaws.com',
+    database: 'Canvas',
+    password: 'csc409place',
+    port: 5432
+})
+
+client.connect(err => {
+    if (err) {
+      console.error('connection error', err.stack)
+    } else {
+      console.log('connected')
+      client.end().then(() => console.log('client has disconnected')).catch(err => console.error('error during disconnection', err.stack))
+    }
+})
+
