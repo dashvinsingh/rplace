f9cc4c374995ed008a8044bcddf0aa5afb8cdca6
Dashvin Singh <circleline10@gmail.com>
2019-12-06 18:18:17 -0500

    renamed server.js

--- place/server.js
+++ place/server.js
@@ -1,172 +1,190 @@
+// Constants
+const VERBOSE = process.argv[2] || false;
+const HTTP_PORT = process.argv[3] || 8080;
+const WS_PORT = parseInt(HTTP_PORT) + 1;
+const DIM = 250;
+
+// Redis
+// var redis = require("redis");
+// const redisClient = redis.createClient();
+
+// Websockets
 const WebSocket = require('ws');
-const dotenv = require('dotenv');
-var redis = require("redis")
-dotenv.config();
+const wss = new WebSocket.Server({ port: WS_PORT });
 
-const env = process.env;
-const wss = new WebSocket.Server({ port: env.WS_PORT });
-const redisClient = redis.createClient("redis://r-place-redis-cache-001.tjlnvm.0001.use2.cache.amazonaws.com", {return_buffer:true});
+// Redis Client
+const redis = require("redis")
+const redis_host = "redis://r-place-redis-cache-001.tjlnvm.0001.use2.cache.amazonaws.com";
 
-var dim = 250; // note: this is not the right dimensions!!
-var board=new Array(dim);
-for(var x=0;x<dim;x++){
-	board[x]=new Array(dim);
-	for(var y=0;y<dim;y++){
-		board[x][y]={ 'r':255, 'g':255, 'b':255 };
+//###Redis Error Handling to be Done.
+const options = {return_buffer: true, retry_strategy:  function(options) {
+	if (options.attempt > 3) {
+		return Error("Unable to connect to redis!");
 	}
-}
-
-wss.on('close', function() {
-    console.log('disconnected');
+}}
+const redisClient = redis.createClient(redis_host, options);
+redisClient.on('error', function (err) {
+    assert(err instanceof Error);
+    assert(err instanceof redis.AbortError);
+    assert(err instanceof redis.AggregateError);
+    // The set and get get aggregated in here
+    console.log("Unable to connect to redis.");
 });
 
+// Set up the board as flat array, each pixel is 1 unsigned integer
+// 3 maps to white, the board will be all white to begin
+let board = new Uint8Array(DIM*DIM); 
+board.fill(3);
+
+// Log when a client disconnects
+wss.on('close', () => {
+	if (VERBOSE) {
+		console.log("\n--------------------------");
+		console.log("DISCONNECTED FROM CLIENT");
+		console.log("--------------------------\n");
+	}
+});
+
+// Broadcast to all connected clients
 wss.broadcast = function broadcast(data) {
+	console.log("->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
+	console.log("Broadcasting message:", data)
+	console.log("->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
   wss.clients.forEach(function each(client) {
     if (client.readyState === WebSocket.OPEN) {
       client.send(data);
-    }
+    } else {
+		console.log("===ERROR: Unable to broadcast, socket state: " + client.readyState);
+	}
   });
 };
 
-
-// for heartbeat to make sure connection is alive 
+// Heartbeat (set isAlive to true on receiving a pong) 
 function noop() {}
 function heartbeat() {
   this.isAlive = true;
 }
 
-function isValidSet(o){
-	var isValid=false;
+// Check if a client pixel update is valid
+function validUpdate(x, y, colour) {
+	let valid = false;
 	try {
-	   isValid = 
-		Number.isInteger(o.x) && o.x!=null && 0<=o.x && o.x<dim &&
-		Number.isInteger(o.y) && o.y!=null && 0<=o.y && o.y<dim && 
-		Number.isInteger(o.r) && o.r!=null && 0<=o.r && o.r<=255 && 
-		Number.isInteger(o.g) && o.g!=null && 0<=o.g && o.g<=255 && 
-		Number.isInteger(o.b) && o.b!=null && 0<=o.b && o.b<=255;
-	} catch (err){ 
-		isValid=false; 
-	} 
-	return isValid;
-}
-
-function send(ws, command, payload) {
-	ws.send(JSON.stringify({command: command, payload: payload}))
-}
-
-function readBytes(byteString) {
-	buff = Buffer.from(byteString);
-	var board = [];
-	for (var i=0; i<buff.length; i++) {
-			board.push(buff[i]);
+		valid = 
+			Number.isInteger(x) && x != null && 0 <= x && x < DIM &&
+			Number.isInteger(y) && y != null && 0 <= y && y < DIM && 
+			Number.isInteger(colour) && colour != null && 0 <= colour && colour <= 15;
+	} catch (err) {
+		console.log("Recieved an invalid update:", err);
+		valid = false;
 	}
-	return board;
+	return valid;
 }
 
-const redisKey = "board";
+// On a new client connection
 wss.on('connection', function(ws) {
-	// heartbeat
-	console.log("New Socket Connection");
-  	ws.isAlive = true;
-  	ws.on('pong', heartbeat);
-	
-	//Client first connects to websocket
-	//Need to send the redit board here.
+	if (VERBOSE) {
+		console.log("\n++++++++++++++++++++++++");
+		console.log("New client connection");
+		console.log("++++++++++++++++++++++++\n");
+	}
+
+  // Heartbeat
+  ws.isAlive = true;
+  ws.on('pong', heartbeat);
+
+	// Send the current board to the client
+	let boardInfo = new Uint8ClampedArray(DIM*DIM + 3);
+	boardInfo[0] = 0;
+	boardInfo[1] = 0;
+	boardInfo[2] = DIM;
+
+
+	//Make connection to redis and send the board back to the clients.
+	const redisKey = "board";
 	redisClient.send_command("GET", [redisKey], function(err, reply) {
-		if (err) {console.log(err); send(ws, "ERROR", {"message": err})};
+		if (err) {console.log("Unable to GET board from Redis. " + err);};
 		if (reply) {
 				console.log(`REDIS GET ${redisKey}, Size: ${reply.length}`);
-				send(ws, "INIT", {board: readBytes(reply)});
+				console.log(reply[0]);
+				boardInfo.set(Buffer.from(reply), 3);
+				ws.send(boardInfo);
 		}
-	});
-
-	// send initial board: this is slow!!!
-	// for (x=0;x<dim;x++){
-	// 	for(y=0;y<dim;y++){
-	// 		var o = { 'x' : x, 'y' : y, 'r': board[x][y].r, 'g': board[x][y].g, 'b': board[x][y].b };
-	// 		ws.send(JSON.stringify(o));
-	// 	}
-	// }
-
-	// when we get a message from the client
+	})
+	// boardInfo.set(board, 3);
+	// ws.send(boardInfo);
+	
+	// On a client update, broadcast that update to all clients
 	ws.on('message', function(message) {
-		var o = JSON.parse(message);
-		switch(o.command) {
-			case "INIT":
-				console.log(o)
-				break;
-			case "WRITE":
-				console.log(o.payload);
-				offset = parseInt(o.payload.x) + parseInt(o.payload.y) * 250;
-				console.log(offset);
-				value = parseInt(o.payload.num);
-				console.log("New data to write from user " + o.payload.user);
-				redisClient.send_command("BITFIELD", [redisKey, "SET", "u8", `#${offset}`, value], function(err, reply) {
-        				if (err) console.log(err);
-        				console.log(reply);
-					wss.broadcast(JSON.stringify({command:"READ", payload:o}));;	
-				});	
-				//Validate if valid input (color validation)
-				//Write to dynamo
-				//Write to redis
-				//Broadcast to all clients
-				// if (isValidSet(o)){
-				// 	wss.broadcast(message);
-				// 	board[o.x][o.y] = { 'r': o.r, 'g': o.g, 'b': o.b };
-				// }
-			default:
-				break;
+		const buffer = Buffer.from(message);
+		let x = buffer[0];
+		let y = buffer[1];
+		let colour = buffer[3];
+
+		if (VERBOSE) {
+			console.log("\n+++++++++++++++++++++++++++++++++++++++");
+			console.log("Got a new message:", x, y, colour);
+			console.log("+++++++++++++++++++++++++++++++++++++++\n");
 		}
 
+		if (validUpdate(x, y, colour)) {
+			if (VERBOSE) {
+				console.log("\n*********************************************");
+				console.log("Updating pixel at ("+x +","+y+") to colour:", colour);
+				console.log("*********************************************\n");
+			}
+
+			// Broadcast this update to each client and store it in the board
+			console.log("New data to write from user " + "{DUMMY}");
+			var index = x + (DIM * y);
+		
+			redisClient.send_command("BITFIELD", [redisKey, "SET", "u8", `#${index}`, colour], function(err, reply) {
+					if (err) console.log(err);
+					if (reply) {
+						console.log("REDIS Write successful");
+						console.log(reply);
+					
+						//broadcast writes to users.
+						board[index] = colour;	
+						wss.broadcast(message);
+					}
+					
+			});	
+			
+		}
 	});
 });
 
-// heartbeat (ping) sent to all clients
+// Heartbeat (ping) sent to all clients
 const interval = setInterval(function ping() {
   wss.clients.forEach(function each(ws) {
     if (ws.isAlive === false) return ws.terminate();
- 
     ws.isAlive = false;
     ws.ping(noop);
   });
 }, 30000);
 
-// Static content
-var express = require('express');
-var app = express();
+// Express server for HTTP
+let express = require('express');
+let app = express();
 
-// static_files has all of statically returned content
-// https://expressjs.com/en/starter/static-files.html
+// Serve static content
 app.use('/',express.static('static_files')); // this directory has files to be returned
 
-
-// Session stuff taken from: https://glebbahmutov.com/blog/express-sessions/
-app.use(require('morgan')('dev'));
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
-
-app.use(session({
-  name: 'server-session-cookie-id',
-  secret: 'my express secret',
-  saveUninitialized: true,
-  resave: true,
-  store: new FileStore()
-}));
-
-
-app.use(function printSession(req, res, next) {
-	console.log('req.session', req.session);
-	return next();
+app.listen(HTTP_PORT, () => {
+	if (VERBOSE) {
+		console.log("\n========================================");
+		console.log('Example app listening on port:', HTTP_PORT);
+		console.log("========================================\n");
+	}
 });
 
+// To DO:
+//   1) Make the board larger (gotta watch out for 8 bit integer limits for x and y values)
+//   2) Use drawImage() and fillRect() and putImageData() maybe
+//   3) Figure out to send binary data through websockets if possible
+//   4) Make a reddit thingie
+//   5) Make a postgres thingie
+//   6) https://gist.github.com/hagino3000/1447986
+//   7) https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array
+//   8) https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/getImageData
 
-
-
-
-
-
-// app.listen(8080, function () {
-//   console.log('Example app listening on port 8080!');
-app.listen(env.APP_PORT, function () {
-  console.log(`Example app listening on port ${env.APP_PORT}!`);
-})
