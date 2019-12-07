395a7501fd498daf90197bd1f91873c2aa77b00a
Bhavjot Singh Baidwan <baidwan2@dh2010pc09.utm.utoronto.ca>
2019-12-07 15:51:25 -0500

    commit before merge

--- place/db.js
+++ place/db.js
@@ -22,8 +22,15 @@ client.connect(err => {
     if (err) {
       console.error('connection error', err.stack)
     } else {
-      console.log('connected')
-      client.end().then(() => console.log('client has disconnected')).catch(err => console.error('error during disconnection', err.stack))
+      console.log('connected, creating tables if not exist')
+      let createCanvas = `CREATE TABLE IF NOT EXISTS Canvas("X" int NOT NULL,"Y" int NOT NULL,"Colour" int NOT NULL,"timestamp" timestamp(6) NOT NULL,Primary KEY ("X", "Y"));`;
+      client.query(createCanvas, function(err, results, fields) {
+        if (err) {
+          console.log(err.message);
+        } else {
+          console.log("table created");
+          console.log(results);
+        }
+      });   
     }
 })
-
--- place/server.js
+++ place/server.js
@@ -1,4 +1,5 @@
 const WebSocket = require('ws');
+require('log-timestamp');
 const wss = new WebSocket.Server({ port: 8081 });
  
 var dim = 250; // note: this is not the right dimensions!!
@@ -65,6 +66,44 @@ wss.on('connection', function(ws) {
 		}
 	});
 });
+
+// https://node-postgres.com/
+require('log-timestamp');
+const { Pool, Client } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'r-place-1.cg7a8becuhbz.us-east-2.rds.amazonaws.com',
+    database: 'Canvas',
+    password: 'csc409place',
+    port: 5432
+})
+
+const client = new Client({
+    user: 'postgres',
+    host: 'r-place-1.cg7a8becuhbz.us-east-2.rds.amazonaws.com',
+    database: 'Canvas',
+    password: 'csc409place',
+    port: 5432
+})
+
+client.connect(err => {
+    if (err) {
+      console.error('connection error', err.stack)
+    } else {
+      console.log('connected, creating tables if not exist')
+      let createCanvas = `CREATE TABLE IF NOT EXISTS Canvas("X" int NOT NULL,"Y" int NOT NULL,"Colour" int NOT NULL,"timestamp" timestamp(6) NOT NULL,Primary KEY ("X", "Y"));`;
+      client.query(createCanvas, function(err, results, fields) {
+        if (err) {
+          console.log(err.message);
+        } else {
+          console.log("table created");
+          console.log(results);
+        }
+      });   
+    }
+})
+
 
 // heartbeat (ping) sent to all clients
 const interval = setInterval(function ping() {
