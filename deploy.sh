cd77d241261e3dd92126b4dc33bbb592fa86fd93
Dashvin Singh <circleline10@gmail.com>
2019-12-07 23:15:40 -0500

    modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 
+
+
 cd place/
+$(aws ecr get-login --no-include-email --region us-east-2)
 docker build -t place-image-repo .
 docker tag place-image-repo 521221603804.dkr.ecr.us-east-2.amazonaws.com/place-image-repo:latest
 docker push 521221603804.dkr.ecr.us-east-2.amazonaws.com/place-image-repo:latest
-aws ecs update-service --cluster place --service place --force-new-deployment
+aws ecs update-service --cluster place --service place-2 --force-new-deployment
